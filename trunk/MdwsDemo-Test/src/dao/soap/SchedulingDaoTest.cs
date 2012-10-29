721a203be83167cfcf4534e6f4446b749749434d
Joel Mewton <joelmewton@gmail.com>
2012-10-29 13:17:57 -0400

    committing scheduling unit tests and DAO

---
+++ trunk/MdwsDemo-Test/src/dao/soap/SchedulingDaoTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MdwsDemo.scheduling;
+
+namespace MdwsDemo.dao.soap
+{
+    [TestFixture]
+    public class SchedulingDaoTest
+    {
+        SchedulingDao _dao;
+        string _accessCode = "5821FOB";
+        string _verifyCode = "6446FDB~";
+
+        [TestFixtureSetUp]
+        public void testFixtureSetUp()
+        {
+            _dao = new SchedulingDao();
+        }
+
+        [TestFixtureTearDown]
+        public void testFixtureTearDown()
+        {
+            _dao.disconnect();
+        }
+
+        void login100()
+        {
+            _dao.connectAndLogin("100", _accessCode, _verifyCode);
+        }
+
+        /// <summary>
+        /// This test illustrates the simple concepts of authenticating your user to a site
+        /// </summary>
+        [Test]
+        public void testConnectAndLogin()
+        {
+            RegionArray siteTable = _dao.getSites();
+            SiteTO selectedSite = null; // placeholder
+            // we already know our site code but, just for completeness, showing walking through the region array here
+            // the VA organizes itself logically and geographically by regions and sites. "regions" is  roughly what 
+            // we term internally as VISNs. There are 23 VISNs in VA - Ann Arbor is in VISN 11. Since MDWS uses this organization
+            // for other things, we modified the terminology slightly
+            // This first foreach loop goes through all the regions in the site table - in our test environment we only have one region
+            foreach (RegionTO region in siteTable.regions)
+            {
+                if (region.sites != null && region.sites.sites != null && region.sites.sites.Length > 0)
+                {
+                    foreach (SiteTO site in region.sites.sites)
+                    {
+                        if (site.sitecode == "100") // we chose a really difficult way of finding our site!
+                        {
+                            selectedSite = site;
+                            break;
+                        }
+                    }
+                }
+                if (selectedSite != null)
+                {
+                    break;
+                }
+            }
+            UserTO user = _dao.connectAndLogin(selectedSite.sitecode, _accessCode, _verifyCode);
+
+            Assert.IsNull(user.fault);
+        }
+
+        [Test]
+        public void testGetClinics()
+        {
+            login100();
+
+            IList<HospitalLocationTO> clinics = _dao.getClinics("A"); // get clinics starting at 'A'
+
+            Assert.IsNotNull(clinics);
+            Assert.IsTrue(clinics.Count > 0);
+            foreach (HospitalLocationTO clinic in clinics)
+            {
+                Assert.IsFalse(String.IsNullOrEmpty(clinic.id), "All clinics need an ID");
+                Assert.IsFalse(String.IsNullOrEmpty(clinic.name), "All clinics need a name");
+            }
+        }
+
+        [Test]
+        public void testGetClinicSchedulingDetails()
+        {
+            login100();
+
+            IList<HospitalLocationTO> clinics = _dao.getClinics("A"); // get clinics starting at 'A'
+
+            // get details for each of the clinics above
+            foreach (HospitalLocationTO clinic in clinics)
+            {
+                HospitalLocationTO clinicWithDetails = _dao.getClinicSchedulingDetails(clinic.id);
+
+                Assert.IsNull(clinicWithDetails.fault);
+                Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.appointmentLength), "Should receive appt length");
+                Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.availability), "Should receive availability info");
+                // one of the clinics in our test Vista doesn't have a display start time specified - artifact of test database...
+                //Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.clinicDisplayStartTime), "Should receive scheduling start time");
+                Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.displayIncrements), "Should receive display increments");
+                Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.name), "Should receive display name");
+                Assert.IsFalse(String.IsNullOrEmpty(clinicWithDetails.type), "Should receive location type (e.g. 'C' for clinic)");
+            }
+        }
+
+        [Test]
+        public void testGetAppointmentTypes()
+        {
+            login100();
+
+            IList<AppointmentTypeTO> apptTypes = _dao.getAppointmentTypes("A");
+
+            foreach (AppointmentTypeTO apptType in apptTypes)
+            {
+                Assert.IsTrue(apptType.active, "All types should be active if returned through this call");
+                Assert.IsFalse(String.IsNullOrEmpty(apptType.id), "Should always received ID");
+                Assert.IsFalse(String.IsNullOrEmpty(apptType.name), "Should always receive name");
+            }
+        }
+
+        [Test]
+        public void testMakeAppointment()
+        {
+            string pid = "91";
+
+            login100();
+            // user is now authenticated
+            IList<AppointmentTypeTO> apptTypes = _dao.getAppointmentTypes("A");
+            // we now have the appointment types we can display to the user
+            IList<HospitalLocationTO> clinics = _dao.getClinics("A"); // get clinics starting at 'A'
+            // we now have a list of clinics we can allow the user to select one
+            HospitalLocationTO clinicWithDetails = _dao.getClinicSchedulingDetails(clinics[0].id);
+            // we now have the details for the selected clinic and can show those to the user so a timeslot can be selected and we can pass the correct params to makeAppointment
+
+            // now ready to make appt - we choose the first appt type and clinic in the returns from above - we also choose to make an appt as soon as the 
+            // clinic opens - the appt length is the length specified for the clinic
+            AppointmentTO scheduledAppt = _dao.makeAppointment(pid, apptTypes[0].id, clinics[0].id, clinicWithDetails.clinicDisplayStartTime, clinicWithDetails.appointmentLength);
+
+            Assert.IsNull(scheduledAppt.fault);
+        }
+    }
+}
---
+++ trunk/MdwsDemo/src/dao/soap/SchedulingDao.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MdwsDemo.scheduling;
+
+namespace MdwsDemo.dao.soap
+{
+    public class SchedulingDao
+    {
+        SchedulingSvc _svc;
+
+        public SchedulingDao()
+        {
+            _svc = new SchedulingSvc();
+            _svc.CookieContainer = new System.Net.CookieContainer();
+        }
+
+        /// <summary>
+        /// Disconnect all Vista connections - never throw an error
+        /// </summary>
+        public void disconnect()
+        {
+            try
+            {
+                _svc.disconnect();
+            }
+            catch (Exception) { }
+        }
+
+        public RegionArray getSites()
+        {
+            RegionArray result = _svc.getVHA();
+            if (result.fault != null)
+            {
+                throw new ApplicationException(result.fault.message);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Connect to a Vista and authenticate the user
+        /// </summary>
+        /// <param name="sitecode">User's site ID (getVHA returns a list of site IDs)</param>
+        /// <param name="username">User's access code</param>
+        /// <param name="password">User's verify code</param>
+        /// <returns>UserTO</returns>
+        public UserTO connectAndLogin(string sitecode, string username, string password)
+        {
+            DataSourceArray connectResult = _svc.connect(sitecode);
+            if (connectResult.fault != null)
+            {
+                throw new ApplicationException(connectResult.fault.message);
+            }
+            UserTO user = _svc.login(username, password, "");
+            if (user.fault != null)
+            {
+                throw new ApplicationException(user.fault.message);
+            }
+            return user;
+        }
+
+        /// <summary>
+        /// Get clinics starting search at target name
+        /// </summary>
+        /// <param name="target">The alphabetical starting point for the clinic list</param>
+        /// <returns>IList of HospitalLocationTO</returns>
+        public IList<HospitalLocationTO> getClinics(string target)
+        {
+            TaggedHospitalLocationArray clinics = _svc.getClinics(target);
+            if (clinics.fault != null)
+            {
+                throw new ApplicationException(clinics.fault.message);
+            }
+            // generic collections are easier to work with - just copying over to list
+            IList<HospitalLocationTO> result = new List<HospitalLocationTO>();
+            foreach (HospitalLocationTO loc in clinics.locations)
+            {
+                result.Add(loc);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the object that contains detailed info (scheduling start time, appointment length, availability info, etc) for a clinic's schedule
+        /// </summary>
+        /// <param name="clinicId">Clinic's ID</param>
+        /// <returns>HospitalLocationTO</returns>
+        public HospitalLocationTO getClinicSchedulingDetails(string clinicId)
+        {
+            HospitalLocationTO result = _svc.getClinicSchedulingDetails(clinicId);
+            if (result.fault != null)
+            {
+                throw new ApplicationException(result.fault.message);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a list of the valid appointment types that can be scheduled. The list begins at the alphabetical search point of target
+        /// </summary>
+        /// <param name="target">The alphabetical start point for the appointment type search</param>
+        /// <returns>IList of AppointmentTypeTO</returns>
+        public IList<AppointmentTypeTO> getAppointmentTypes(string target)
+        {
+            AppointmentTypeArray apptTypes = _svc.getAppointmentTypes(target);
+            if (apptTypes.fault != null)
+            {
+                throw new ApplicationException(apptTypes.fault.message);
+            }
+            IList<AppointmentTypeTO> result = new List<AppointmentTypeTO>();
+            foreach (AppointmentTypeTO apptType in apptTypes.appointmentTypes)
+            {
+                result.Add(apptType);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieve patients with a scheduled appointment in a clinic. Notice: data structure may be modified to accomodate appointment information
+        /// </summary>
+        /// <param name="clinicId">Clinic's ID</param>
+        /// <param name="startDate">Beginning date</param>
+        /// <param name="stopDate">Stop date</param>
+        /// <returns></returns>
+        public IList<PatientTO> getPatientsByClinic(string clinicId, string startDate, string stopDate)
+        {
+            PatientArray result = _svc.getPatientsByClinic(clinicId, startDate, stopDate);
+            if (result.fault != null)
+            {
+                throw new ApplicationException(result.fault.message);
+            }
+            IList<PatientTO> patients = new List<PatientTO>();
+            foreach (PatientTO patient in result.patients)
+            {
+                patients.Add(patient);
+            }
+            return patients;
+        }
+
+        public AppointmentTO makeAppointment(string pid, string apptType, string clinicId, string apptTimestamp, string apptLength)
+        {
+            selectPatient(pid);
+            AppointmentTO scheduledAppt = _svc.makeAppointment(clinicId, apptType, apptTimestamp, apptLength);
+            if (scheduledAppt.fault != null)
+            {
+                throw new ApplicationException("Unable to make appointment: " + scheduledAppt.fault.message);
+            }
+            return scheduledAppt;
+        }
+
+        // we're going to use the EmrSvc facade to select our patient since the select function isn't exposed on SchedulingSvc
+        internal void selectPatient(string pid)
+        {
+            MdwsDemo.mdws.EmrSvc emrSvc = new mdws.EmrSvc();
+            emrSvc.CookieContainer = _svc.CookieContainer; // share state
+            emrSvc.select(pid);
+        }
+    }
+}
